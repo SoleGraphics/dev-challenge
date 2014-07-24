7a6f5de4b2748e8297dc214ab7eb09476d0fd198
Grant Regimbal <Regimbal.grant@gmail.com>
2014-07-23 18:32:11 -0700

    finished strpos

--- challenges/strpos/strpos.php
+++ challenges/strpos/strpos.php
@@ -1,8 +1,28 @@
 <?php
 
-function my_strpos(/* arguments go here! */)
+function my_strpos($whole_string, $chunk_string, $position_check=false)
 {
-	# Your code goes here!
+	$chunk_string_char = str_split($chunk_string)[0];
+	$whole_string_array = str_split($whole_string);
+
+	if ($position_check != false) {
+		if ($chunk_string_char == $whole_string_array[$position_check]) {
+			return true;
+		}
+
+		return false;
+	}
+
+	$array_position = 0;
+	foreach ($whole_string_array as $character) {
+		if ($character == $chunk_string_char) {
+			return $array_position;
+		}
+		$array_position = $array_position + 1;
+	}
+	return false;
+
+
 }
 
 $alphabet = 'abcdefghijklmnopqrstuvwxyz';
@@ -18,3 +38,5 @@ var_dump(my_strpos($alphabet, 'u', 22));
 
 # Should print "bool(false)"
 var_dump(my_strpos($alphabet, 'A'));
+
+
